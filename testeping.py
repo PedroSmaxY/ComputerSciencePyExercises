626516684be2bc02460691001e6b93afd7ac0cc3
Pedro Henrique <pedrohnovais@outlook.com>
2023-11-02 09:53:41 -0300

    Update testeping.py

--- testeping.py
+++ testeping.py
@@ -1,7 +1,7 @@
 import subprocess
 
 while True:
-    endereco_ip = "26.180.235.84" # endereço IP que deseja testar o ping
+    endereco_ip = " " # endereço IP que deseja testar o ping
 
     retorno = subprocess.call(["ping", "-c", "3", endereco_ip]) # envia um ping com 3 pacotes
 
